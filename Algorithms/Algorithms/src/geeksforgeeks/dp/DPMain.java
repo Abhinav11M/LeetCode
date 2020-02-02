b1ed1ba22326d1af2527aaf641a4778133c13245
Abhinav <aabhinav11m@gmail.com>
2020-02-02 23:51:01 +0530

    Added more DP algos from GeeksForGeeks.

--- Algorithms/Algorithms/src/geeksforgeeks/dp/DPMain.java
+++ Algorithms/Algorithms/src/geeksforgeeks/dp/DPMain.java
@@ -23,5 +23,81 @@ public class DPMain {
 		System.out.println(dp.binomialCoeff(7,3));
 		System.out.println(dp.binomialCoeff(9,4));
 		// ===== Binomial Coefficient =====
+		
+		// ===== Permutation Coefficient =====
+		System.out.println(dp.permutationCoeff(10, 3));
+		System.out.println(dp.permutationCoeff(7,2));
+		// ===== Permutation Coefficient =====
+		
+		// ===== Tiling Problem =====
+		System.out.println(dp.numWaysToTile(4));
+		System.out.println(dp.numWaysToTile(5));
+		System.out.println(dp.numWaysToTile(6));
+		// ===== Tiling Problem =====
+
+		// ===== Gold mine problem =====
+		System.out.println(dp.getMaxGold( new int[][] { {1,2}, {3,4} } ));
+		System.out.println(dp.getMaxGold( new int[][] { {1,3,3}, {2,1,4}, {0,6,4} } ));
+		System.out.println(dp.getMaxGold( new int[][] { 
+			{1, 3, 1, 5}, 
+			{2, 2, 4, 1}, 
+			{5, 0, 2, 3}, 
+			{0, 6, 1, 2} 
+		} ));
+		
+		System.out.println(dp.getMaxGold( new int[][] { 
+			{1, 1, 12}, 
+			{9, 3, 8}, 
+			{12, 7, 10}
+		} ));
+		
+		System.out.println(dp.getMaxGoldDP( new int[][] { 
+			{1, 3, 1, 5}, 
+			{2, 2, 4, 1}, 
+			{5, 0, 2, 3}, 
+			{0, 6, 1, 2} 
+		} ));
+		
+		System.out.println(dp.getMaxGoldDP( new int[][] { 
+			{1, 1, 12}, 
+			{9, 3, 8}, 
+			{12, 7, 10}
+		} ));
+		
+		System.out.println(dp.getMaxGoldDP( new int[][] { 
+			{1, 4, 101, 5}, 
+			{50, 3, 9, 4}, 
+			{9, 8, 3, 9}, 
+			{7, 1, 5, 7} 
+		} ));
+		
+		// This algo is not correct
+		System.out.println(dp.getMaxGold( new int[][] { 
+			{1, 4, 101, 5}, 
+			{50, 3, 9, 4}, 
+			{9, 8, 3, 9}, 
+			{7, 1, 5, 7} 
+		} ));
+		// ===== Gold mine problem =====
+		
+		// ===== Coin change problem =====
+		System.out.println(dp.countCoins(new int[]{2, 3, 5, 6}, 10));
+		System.out.println(dp.countCoins(new int[]{1, 2, 3}, 4));
+
+		System.out.println(dp.countCoinsDP(new int[]{2, 3, 5, 6}, 10));
+		System.out.println(dp.countCoinsDP(new int[]{1, 2, 3}, 4));
+		// ===== Coin change problem =====
+		
+		// ===== Friends Pairing Problem =====
+		System.out.println(dp.countFriendsPairings(3));
+		System.out.println(dp.countFriendsPairings(4));
+		System.out.println(dp.countFriendsPairings(5));
+		// ===== Friends Pairing Problem =====
+		
+		// ===== Subset Sum Problem in O(sum) space =====
+		System.out.println(dp.isSubsetSum(new int[] {4, 1, 10, 12, 5, 2}, 9));
+		System.out.println(dp.isSubsetSum(new int[] {1, 8, 2, 5}, 4));
+		// ===== Subset Sum Problem in O(sum) space =====
+		
 	}
 }
--- Algorithms/Algorithms/src/geeksforgeeks/dp/DPQuestions.java
+++ Algorithms/Algorithms/src/geeksforgeeks/dp/DPQuestions.java
@@ -214,4 +214,218 @@ public class DPQuestions {
 		return tab[r];
 	}
 	
+	/**
+	 * Permutation coefficient
+	 * Recurrence Relation : nPr = (n-1)P(r) + r*(n-1)P(r-1)
+	 */
+
+	int permutationCoeff(int n, int k) {
+		
+		if(n == 0 || k == 0) {
+			return 1;
+		}
+	
+		// Create a table for n[p][r]
+		int[][] data = new int[n+1][n+1];
+		
+		// nP0 is always 1
+		for(int i = 0; i < n; ++i) {
+			data[i][0] = 1;
+		}
+		
+		
+		for(int i = 1; i <= n; ++i) {
+			for(int j = 1; j <= Math.min(i, k); ++j) { // Only need to calculate till nPk.. Don't need to go till nPn.
+				data[i][j] = data[i-1][j] + j*data[i-1][j-1];
+			}
+		}
+		
+		return data[n][k];
+	}
+	
+	/**
+	 * Tiling Problem
+	 * <a>https://www.geeksforgeeks.org/tiling-problem/</a>
+	 */
+	public int numWaysToTile(int n) {
+		if(n == 1 || n == 2) {
+			return n;
+		}
+		
+		int[] data = new int[n];
+		data[0] = 1; // for n = 1
+		data[1] = 2; // for n = 2. So index i-1
+		
+		for(int i = 2; i < n; ++i) {
+			data[i] = data[i-1] + data[i-2];
+		}
+		
+		return data[n-1];
+	}
+	
+	/**
+	 * Gold Mine Problem => Incorrect
+	 * Recursive approach (DP optimized below)
+	 * TODO : Correct this recursive algo
+	 */
+	public int getMaxGold(int[][] mine) {
+		// We don't know the starting position. So we will have to try for all the indexes in column 0.
+		int max = Integer.MIN_VALUE;
+		for(int r = 0; r < mine.length; ++r) {
+			int maxGold = getMaxGold(mine, r, 0, mine[r][0]);
+			max = maxGold > max ? maxGold : max;
+		}
+		
+		return max;
+	}
+
+	private int getMaxGold(int[][] mine, int r, int c, int goldMax) {
+		if(c == mine.length-1) { // Reached the end of column. Can't move
+			// Return max from the cols
+			return goldMax;
+		}
+		
+		// find where to move
+		int rightUp = Integer.MIN_VALUE, right = Integer.MIN_VALUE, rightDown = Integer.MIN_VALUE;
+
+		if(r-1 >= 0) {
+			rightUp = mine[r-1][c+1];
+		}
+		right = mine[r][c+1];
+		if(r+1 < mine[0].length) {
+			rightDown = mine[r+1][c+1];
+		}
+		
+		int max = Math.max(rightUp, Math.max(right, rightDown));
+		
+		if(max == rightUp) {
+			return getMaxGold(mine, r-1, c+1, goldMax+max);
+		}
+		else if(max == right) {
+			return getMaxGold(mine, r, c+1, goldMax+max);
+		}
+		else {
+			return getMaxGold(mine, r+1, c+1, goldMax+max);
+		}
+	}
+	
+	public int getMaxGoldDP(int[][] mine) {
+		int nRows = mine.length;
+		int nCols = mine[0].length;
+		
+		int[][] maxArr = new int[nRows][nCols];
+		
+		// Initialize last col with the mine values
+		for(int i = 0; i < nRows; ++i) {
+			maxArr[i][nCols-1] = mine[i][nCols-1];
+		}
+		
+		for(int c = nCols-2; c >= 0; --c) {
+			for(int r = 0; r <= nRows-1; ++r) {
+				int rightUp = Integer.MIN_VALUE, rightDown = Integer.MIN_VALUE;
+				
+				if(r-1 >= 0) {
+					rightUp = maxArr[r-1][c+1];
+				}
+				if(r+1 <= nRows-1) {
+					rightDown = maxArr[r+1][c+1];
+				}
+				
+				maxArr[r][c] = mine[r][c] + maxNext(rightUp, maxArr[r][c+1], rightDown);
+			}
+		}
+		
+		int maxGold = Integer.MIN_VALUE;
+		for(int r = 0; r < nRows; ++r) {
+			maxGold = maxArr[r][0] > maxGold ? maxArr[r][0] : maxGold;
+		}
+		
+		return maxGold;
+	}
+
+	private int maxNext(int up, int right, int down) {
+		return Math.max(up, Math.max(right, down));
+	}
+	
+	/**
+	 * Coin change problem
+	 */
+	
+	public int countCoins(int coins[], int sum) {
+		return countCoins(coins, sum, 0);
+	}
+
+	private int countCoins(int[] coins, int sum, int coinIdx) {
+		if(sum == 0) {
+			return 1;
+		}
+		if((coinIdx >= coins.length) || (sum-coins[coinIdx] < 0)) {
+			return 0;
+		}
+		
+		return countCoins(coins, sum-coins[coinIdx], coinIdx) + countCoins(coins, sum, coinIdx+1);
+	}
+	
+	// Using DP
+	public int countCoinsDP(int[] coins, int sum) {
+		int[][] data = new int[sum+1][coins.length];
+		
+		for(int i = 0; i < coins.length; ++i) {
+			data[0][i] = 1;
+		}
+
+		
+		for(int i = 1; i <= sum; ++i) {
+			for(int j = 0; j < coins.length; ++j) {
+				// Count including the coin at index j
+				int x = (i-coins[j] >= 0) ? data[i-coins[j]][j] : 0;
+				
+				// Count excluding the coin at index j
+				int y = (j >= 1) ? data[i][j-1] : 0;
+				
+				data[i][j] = x+y;
+			}
+		}
+		
+		return data[sum][coins.length-1];
+	}
+	
+	/**
+	 * Friends Pairing Problem
+	 * f(n) = f(n-1) + (n-1) * f(n-2)
+	 */
+	public int countFriendsPairings(int n) {
+		int data[] = new int[n+1];
+		data[0] = 1;
+		data[1] = 1;
+		
+		for(int i = 2; i <= n; ++i) {
+			data[i] = data[i-1] + (i-1) * data[i-2];
+		}
+		
+		return data[n];
+	}
+	
+	/**
+	 * Subset Sum Problem in O(sum) space
+	 * TODO: Write the DP algo
+	 */
+	boolean isSubsetSum(int arr[], int sum) {
+//		Arrays.sort(arr); // If we don't sort, then we can't check sum < arr[idx]
+		return isSubsetSum(arr, sum, 0);
+	}
+
+	private boolean isSubsetSum(int[] arr, int sum, int idx) {
+		if(sum == 0) {
+			return true;
+		}
+		
+//		if(idx >= arr.length|| sum < arr[idx]) {
+		if(idx >= arr.length) {
+			return false;
+		}
+		
+		// Including int at idx || excluding int at idx
+		return (isSubsetSum(arr, sum-arr[idx], idx+1) || isSubsetSum(arr, sum, idx+1));
+	}
 }
