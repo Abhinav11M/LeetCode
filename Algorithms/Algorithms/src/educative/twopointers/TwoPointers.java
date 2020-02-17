350b6b513c7a7f1361feb7add0f03ab4720fbec9
Abhinav <aabhinav11m@gmail.com>
2020-02-17 23:44:26 +0530

    Added more educative two pointers questions.

--- Algorithms/Algorithms/src/educative/twopointers/TwoPointers.java
+++ Algorithms/Algorithms/src/educative/twopointers/TwoPointers.java
@@ -1,5 +1,10 @@
 package educative.twopointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TwoPointers {
 	
 	/**
@@ -94,4 +99,221 @@ public class TwoPointers {
 		return res;
 	}
 	
+	/**
+	 * Triplet Sum to Zero (medium)
+	 */
+	public List<List<Integer>> TripletSumToZero(int[] arr) {
+		List<List<Integer>> res = new ArrayList<>();
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length-2; ++i) {
+			// This is to handle duplicates. If we have -2, -2 at the front, we will have two same solutions.
+			if(i != 0 && arr[i] == arr[i-1]) {
+				continue;
+			}
+
+			int j = i+1;
+			int k = arr.length-1; // Pointer from the end
+			while(k > j) {
+				if(arr[i] + arr[j] + arr[k] == 0) {
+					res.add(Arrays.asList(new Integer[] {arr[i], arr[j], arr[k]}));
+					j++;
+					k--;
+					
+					// This is to handle duplicates, if we have 2,2 at the end, we will end up with 2 same solutions.
+					// So adjust the pointers to point to different values than the previous one.
+					while(j < k && arr[j] == arr[j-1]) {
+						j++;
+					}
+					while(k > j && arr[k] == arr[k+1]) {
+						k--;
+					}
+				}
+				else if(arr[i] + arr[j] + arr[k] < 0) { // Negatives are larger, so shift j towards right
+					j++;
+				}
+				else { // Positive is more, so shift k towards left
+					k--;
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	/**
+	 * Triplet Sum Close to Target (medium)
+	 */
+	public int tripletSumCloseToTarget(int[] arr, int targetSum) {
+		Arrays.sort(arr);
+		
+		int difference = Integer.MAX_VALUE;
+		int retTarget = 0;
+		
+		for(int i = 0; i < arr.length-2; ++i) {
+			// Ignore duplicates
+			if(i != 0 && arr[i] == arr[i-1]) {
+				continue;
+			}
+
+			int left = i+1;
+			int right = arr.length-1;
+			
+			while(left < right) {
+				if(arr[i] + arr[left] + arr[right] == targetSum) {
+					return targetSum;
+				}
+				else if(arr[i] + arr[left] + arr[right] > targetSum) {
+					int tempDifference = Math.abs(targetSum - (arr[i] + arr[left] + arr[right]));
+					if(tempDifference < difference) {
+						difference = tempDifference;
+						retTarget = arr[i] + arr[left] + arr[right];
+					}
+					// Try to move closer towards the sum
+					--right;
+				}
+				else {
+					int tempDifference = Math.abs(targetSum - (arr[i] + arr[left] + arr[right]));
+					if(tempDifference < difference) {
+						difference = tempDifference;
+						retTarget = arr[i] + arr[left] + arr[right];
+					}
+					// Try to move closer towards the sum
+					++left;
+				}
+			}
+		}
+		
+		return retTarget;
+	}
+	
+	/**
+	 * Triplets with Smaller Sum
+	 */
+	public int tripletWithSmallerSum(int[] arr, int target) {
+		int count = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length-2; ++i) {
+			// Avoiding duplicates
+			if(i != 0 && arr[i] == arr[i-1]) {
+				continue;
+			}
+
+			int left = i+1;
+			int right = arr.length-1;
+			
+			while(left < right) {
+				if(arr[i] + arr[left] + arr[right] >= target) {
+					// We need a smaller sum, so move right towards left
+					--right;
+					// Avoid duplicates
+					while(right > left && arr[right] == arr[right+1]) {
+						--right;
+					}
+				}
+				else {
+					++count;
+					for(int j = right-1; j > left; --j) {
+						if(arr[j] == arr[j+1]) {
+							continue;
+						}
+						else {
+							++count;
+						}
+					}
+					
+					++left;
+					// Avoid duplicates
+					while(left < right && arr[left] == arr[left-1]) {
+						++left;
+					}
+				}
+			}
+		}
+		
+		return count;
+	}
+	
+	/**
+	 * Subarrays with Product Less than a Target 
+	 */
+	public List<List<Integer>> subarrayProductLessThanKOpt(int[] arr, int target) {
+		List<List<Integer>> res = new ArrayList<>();
+		
+		int left = 0, product = 1;
+		for(int right = 0; right < arr.length; ++right) {
+			product *= arr[right];
+			
+			// If the product exceeds the product, move the left window towards right.
+			while(product >= target) {
+				product /= arr[left++];
+			}
+			
+			// All the subsets between left and right is a required subArray.
+			// To avoid duplicates, we will have to construct the subArrays from right to left.
+			List<Integer> l = new LinkedList<>();
+			for(int i = right; i >= left; --i) {
+				l.add(0, arr[i]);
+				res.add(new ArrayList<>(l));
+			}
+		}
+		
+		return res;
+	}
+	
+	/**
+	 * This is not optimized (Brute force)
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public int subarrayProductLessThanKOpt_leetCode(int[] nums, int k) {
+		int product = 1;
+		int counter = 0;
+		
+		for(int i = 0; i < nums.length; ++i) {
+			product = nums[i];
+			if(product >= k) {
+                product /= nums[i];
+				continue;
+			}
+			++counter; // Just nums[i]
+            
+			for(int j = i+1; j < nums.length; ++j) {
+				product *= nums[j];
+				if(product >= k) {
+                    product /= nums[j];
+                    break;
+				}
+				++counter;
+			}
+		}
+		
+		return counter;
+	}
+	
+	public int subarrayProductLessThanKOpt_leetCode_Opt(int[] nums, int k) {
+		int left = 0;
+		int product = 1;
+		int counter = 0;
+		
+		for(int right = 0; right < nums.length; ++right) {
+			product *= nums[right];
+			
+			while(product >= k && left <= right) {
+				product /= nums[left++];
+			}
+			
+			// If still the product is larger, move to next : When target = 0, and inputs are all positive
+			if(product > k) {
+				continue;
+			}
+			
+			counter += right-left+1;
+		}
+		
+		return counter;
+	}
 }
--- Algorithms/Algorithms/src/educative/twopointers/TwoPointersMain.java
+++ Algorithms/Algorithms/src/educative/twopointers/TwoPointersMain.java
@@ -12,8 +12,18 @@ public class TwoPointersMain {
 		System.out.println(twoPointers.removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
 		
 		Helpers.printArr(twoPointers.makeSortedArraySquares(new int[] {-2, -1, 0, 2, 3}));
-	}
-	
-	
 
+		System.out.println(twoPointers.TripletSumToZero(new int[] {-2, -1, 0, 2, 3}));
+
+		System.out.println(twoPointers.tripletSumCloseToTarget(new int[] {-2, 0, 1, 2}, 2));
+		System.out.println(twoPointers.tripletSumCloseToTarget(new int[] {-1,2,1,-4}, 1));
+		
+		System.out.println(twoPointers.tripletWithSmallerSum(new int[] {-1, 4, 2, 1, 3}, 5));
+		
+//		System.out.println(twoPointers.subarrayProductLessThanK(new int[] {1,2,3,4,5}, 5));
+		System.out.println(twoPointers.subarrayProductLessThanKOpt(new int[] {1,2,3,4,5}, 5));
+		System.out.println(twoPointers.subarrayProductLessThanKOpt(new int[] {10,9,10}, 60));
+		
+		System.out.println(twoPointers.subarrayProductLessThanKOpt_leetCode(new int[] {10,9,10,4,3,8,3,3,6,2,10,10,9,3}, 19));
+	}
 }
